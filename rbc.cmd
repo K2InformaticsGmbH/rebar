89b66c0399f6a45caa6d9153ae88161e89b996ed
Stefan Ochsenbein <ochsenbein@bluewin.ch>
2015-05-01 16:41:12 +0200

    support skip_deps / skip_deps=true / skip_deps=module,module

--- rbc.cmd
+++ rbc.cmd
@@ -1,2 +1,31 @@
 @echo off
+
+if "%1%"=="" goto raw
+if "%2%"=="" goto skip
+if "%3%"=="" goto single
+if "%4%"=="" goto double
+if "%5%"=="" goto triple
+
+call rebar %1%=%2%,%3%;%4%;%5% compile
+goto end
+
+:triple
+call rebar %1%=%2%,%3%;%4% compile
+goto end
+
+:double
+call rebar %1%=%2%,%3% compile
+goto end
+
+:single
+call rebar %1%=%2% compile
+goto end
+
+:skip
+call rebar %1%=true compile
+goto end
+
+:raw
 call rebar compile
+
+:end
